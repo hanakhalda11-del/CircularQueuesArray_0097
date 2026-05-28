631636c076708cd74e45f87ee8efd7976294d960
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-28 20:27:12 +0700

    Format and update CircularQueuesArray.cpp

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -22,4 +22,17 @@ public:
         cin >> num;
         cout << endl;
 
-        
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "Queue overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+       
